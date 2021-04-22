5d77d5f041c619ac27b6e351ae1488b8699628ea
BarCzerw <czerwonkabartlomiej@gmail.com>
2021-04-22 20:47:57 +0200

    badWeatherApp.ui.appLauncher.BadWeatherApp - commit w h2database

--- src/main/java/badWeatherApp/ui/appLauncher/BadWeatherApp.java
+++ src/main/java/badWeatherApp/ui/appLauncher/BadWeatherApp.java
@@ -5,6 +5,7 @@ public class BadWeatherApp {
     public static void main(String[] args) {
 
         System.out.println("Branch sprint 1");
+        System.out.println("Branch h2database");
         System.out.println("Running");
 
     }
